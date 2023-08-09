1a6deabea2163b91f92dca71c6abadbd9f8686f3
Idalis Valadez <idalisvaladez22@gmail.com>
2023-08-08 19:38:50 -0500

    Complete lab

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const cats = [
+/* const cats = [
     "Milo",
     "Otis",
     "Garfield",
@@ -42,3 +42,50 @@ function removeFirstCat() {
     const catsCopy = cats.slice(1);
     return catsCopy;
 }
+*/
+
+
+
+const cats = ['Milo', 'Otis', 'Garfield']
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat(name) {
+    cats.pop(name)
+}
+
+function destructivelyRemoveFirstCat(name) {
+    cats.shift(name)
+}
+
+function appendCat(name) {
+    const newArray = [...cats, name]
+    return newArray
+}
+
+function prependCat(name) {
+    const newArr = [name, ...cats]
+    return newArr
+}
+
+function removeLastCat(name) {
+    const newArr = cats.slice(0,2)
+    return newArr
+}
+function removeFirstCat(name) {
+    const newArray = cats.slice(1)
+    return newArray
+}
+
+
+
+
+
+
+
